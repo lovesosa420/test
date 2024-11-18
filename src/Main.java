d9fc3471c82cb5136e71c56f3bd9357576427e93
Georgii Proshkin <trappapa420@mail.ru>
2024-11-18 16:50:48 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(removeWhiteSpaces("А роза упала на лапу Азора"));
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
     }
 
     private static String removeWhiteSpaces(String str) {
