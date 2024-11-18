5027439277f6fe69179c7159aa416f0ad1be52f2
Georgii Proshkin <trappapa420@mail.ru>
2024-11-18 15:59:14 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(removeWhiteSpaces("А роза упала на лапу Азора"));
+    }
+
+    private static String removeWhiteSpaces(String str) {
+        return  str.replaceAll(" ", "");
+    }
+}
